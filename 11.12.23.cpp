b2e410018b5c5d72ce26fb69efdc7dbf8a9f76d4
frol1k3 <133392305+frol1k3@users.noreply.github.com>
2024-01-19 00:36:47 +0300

    Add files via upload

---
+++ 11.12.23.cpp
@@ -0,0 +1,16 @@
+﻿#include <forward_list>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    forward_list<int> nums{ 5, 9, 24, 8, 25, 3 };
+    nums.remove_if([](int n) {return n % 3 == 0; });
+    for (auto i = nums.begin(); i != nums.end(); i++) {
+        cout << *i << " ";
+    }
+    cout << endl;
+}
